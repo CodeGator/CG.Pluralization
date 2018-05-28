a82d2aa52f078517d449ff1ebd49a7f1378b7d3a
Andrew Arnott <andrewarnott@gmail.com>
2018-05-28 14:02:41 -0700

    Support .NET Standard 1.0
    This was fairly trivial -- only two methods were missing which were easily implemented as extension methods in order to not touch the original code at all.
    
    I also removed the explicit netcoreapp2.0 targeted build since nothing was added to it over the netstandard2.0 version.

---
+++ CG.Pluralization/PolyfillUtilities.cs
@@ -0,0 +1,45 @@
+﻿//---------------------------------------------------------------------
+// <copyright file="PolyfillUtilities.cs" company="Andrew Arnott">
+//      Copyright (c) Andrew Arnott.  All rights reserved.
+// </copyright>
+//
+// @owner       Andrew Arnott
+//---------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+
+namespace CG.Pluralization
+{
+    internal static class PolyfillUtilities
+    {
+#if !NETSTANDARD2_0
+        internal static string ToLower(this string value, CultureInfo cultureInfo)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (cultureInfo == null)
+            {
+                throw new ArgumentNullException(nameof(cultureInfo));
+            }
+
+            return cultureInfo.TextInfo.ToLower(value);
+        }
+
+        internal static bool EndsWith(this string value, string suffix, bool ignoreCase, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            culture = culture ?? CultureInfo.CurrentCulture;
+
+            return culture.CompareInfo.IsSuffix(value, suffix, ignoreCase ? CompareOptions.IgnoreCase : CompareOptions.None);
+        }
+#endif
+    }
+}
